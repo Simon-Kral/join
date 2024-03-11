a1699e68161f9c664fc3a4feb87bf8c96524819a
JohnBroda <161051910+JohnBroda@users.noreply.github.com>
2024-03-11 18:14:51 +0100

    Register V0.9

--- js/register.js
+++ js/register.js
@@ -1,36 +1,113 @@
 let users = [];
 
-async function register(){
-    if(register_password.value != register_confirm_password.value) {
+async function register() {
+    if (register_password.value != register_confirm_password.value) {
         register_confirm_password.setCustomValidity("Passwords Don't Match");
     }
-    else{
-    users.push({
-        name: register_name.value,
-        email: register_email.value,
-        password: register_password.value,
-    })
-    resetForm()
-}
+    else {
+        users.push({
+            name: register_name.value,
+            email: register_email.value,
+            password: register_password.value,
+        })
+        resetForm()
+    }
 }
 
-function resetForm(){
-    register_name.value = '';
-    register_email.value = '';
-    register_password.value = '';
-    register_confirm_password.value = '';
-    register_checkbox.checked = false
+function resetForm() {
+    registerValueEmpty()
     backToLogin()
     singedUpSucces()
 }
 
-function singedUpSucces(){
+function registerValueEmpty() {
+    register_name.value = '';
+    register_email.value = '';
+    register_pw_sign_in.value = '';
+    register_pw_sign_in_confirm.value = '';
+    register_checkbox.checked = false
+}
+
+function singedUpSucces() {
     let singedupsucces = document.getElementById('singed_Up_Succes')
-    singedupsucces.classList.replace('d-none','singed-Up-Succes')
+    singedupsucces.classList.replace('d-none', 'singed-Up-Succes')
     setTimeout(singedUpSuccesRemove, 2000)
 }
 
 function singedUpSuccesRemove() {
     let singedupsucces = document.getElementById('singed_Up_Succes')
-    singedupsucces.classList.replace('singed-Up-Succes','d-none')
+    singedupsucces.classList.replace('singed-Up-Succes', 'd-none')
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.login-input input').forEach(function (input) {
+        let logininput = input.closest('.login-input');
+
+        input.addEventListener('focus', function () {
+            logininput.style.borderColor = 'rgb(41, 171, 226)';
+        });
+
+        input.addEventListener('blur', function () {
+            logininput.style.borderColor = '#D1D1D1';
+        });
+    });
+});
+
+function changeLoginImg() {
+    let loginpwimg = document.getElementById('register_pw_login_img')
+
+    if (loginpwimg.src.endsWith('assets/img/register_lock.png')) {
+        loginpwimg.src = 'assets/img/visibility_off.png'
+    }
+}
+
+function changeSigninImg() {
+    let signinimg = document.getElementById('register_pw_sign_in_img')
+    let signinimgconfirm = document.getElementById('register_pw_sign_in_confirm_img')
+
+    if (signinimg.src.endsWith('assets/img/register_lock.png')) {
+        signinimg.src = 'assets/img/visibility_off.png';
+        signinimgconfirm.src = 'assets/img/visibility_off.png'
+    }
+    if (signinimgconfirm.src.endsWith('assets/img/register_lock.png')) {
+        signinimg.src = 'assets/img/visibility_off.png';
+        signinimgconfirm.src = 'assets/img/visibility_off.png'
+    }
+}
+
+function encryptPassword1() {
+    let loginpw = document.getElementById('register_pw_login_value')
+    let loginpwimg = document.getElementById('register_pw_login_img')
+
+    if (loginpwimg.src.endsWith('assets/img/visibility_off.png')) {
+        loginpw.type = 'text';
+        loginpwimg.src = 'assets/img/visibility.png';
+    }
+    else {
+        loginpw.type = 'password';
+        loginpwimg.src = 'assets/img/visibility_off.png';
+    }
+}
+
+function encryptPassword2() {
+    let signinpw = document.getElementById('register_pw_sign_in')
+    let signinpimg = document.getElementById('register_pw_sign_in_img')
+    let signinpwconfirm = document.getElementById('register_pw_sign_in_confirm')
+    let signinpwconfirmimg = document.getElementById('register_pw_sign_in_confirm_img')
+    encryptPassword2IfElse(signinpw, signinpimg, signinpwconfirm, signinpwconfirmimg)
+}
+
+function encryptPassword2IfElse(signinpw, signinpimg, signinpwconfirm, signinpwconfirmimg) {
+    if (signinpimg.src.endsWith('assets/img/visibility_off.png')) {
+        signinpw.type = 'text';
+        signinpwconfirm.type = 'text';
+        signinpimg.src = 'assets/img/visibility.png';
+        signinpwconfirmimg.src = 'assets/img/visibility.png';
+    }
+    else {
+        signinpw.type = 'password';
+        signinpwconfirm.type = 'password';
+        signinpimg.src = 'assets/img/visibility_off.png';
+        signinpwconfirmimg.src = 'assets/img/visibility_off.png';
+    }
 }
