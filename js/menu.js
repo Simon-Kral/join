b5055b164381024814737739f75b8c3259c058b5
JohnBroda <161051910+JohnBroda@users.noreply.github.com>
2024-03-13 12:59:35 +0100

    Menu open and close

--- js/menu.js
+++ js/menu.js
@@ -7,3 +7,37 @@ async function loadmenu() {
         menuuser.innerHTML = username
     }
 }
+
+function openMenu() {
+    let openmenu = document.getElementById('menu_open');
+    openmenu.innerHTML = menuInnerHTML()
+    closeMenuE(openmenu)
+}
+
+function closeMenuE(openmenu) {
+    function closeMenu(event) {
+        let removemenu = openmenu.contains(event.target);
+        if (!removemenu) {
+            openmenu.innerHTML = '';
+            document.removeEventListener('click', closeMenu);
+        }
+    }
+    setTimeout(function () {
+        document.addEventListener('click', closeMenu);
+    }, 0);
+}
+
+function menuInnerHTML() {
+    let menu = `        
+    <div class="open-menu">
+        <a href="privacy_policy.html" class="open-menu-up"> <p>Privacy Policy</p> </a>
+        <a href="legal_notice.html" class="open-menu-mid"> <p>Legal notice</p> </a>
+        <a href="index.html" class="open-menu-down" onclick="logOut()"> <p>Log out</p> </a>
+    </div>`;
+    return menu
+}
+
+function logOut() {
+    loaduser.splice(loadUsers)
+    localStorage.setItem('userI', loaduser)
+}
