cf44213c06d823633d570151de6959822e8ede9e
JohnBroda <161051910+JohnBroda@users.noreply.github.com>
2024-03-17 11:19:29 +0100

    Register Handy animation

--- js/homepage.js
+++ js/homepage.js
@@ -1,21 +1,3 @@
-function loadRegister() {
-
-    setTimeout(loadRegisterTimeout, 500)
-    setTimeout(loadRegisterTimeoutIndex, 1500)
-}
-
-function loadRegisterTimeout() {
-    let startregisterbackground = document.getElementById('start_register_background')
-    let startregisterbox = document.getElementById('start_register_box')
-
-    startregisterbox.classList.replace('start-register-box','start-register-box-small')
-    startregisterbackground.classList.replace('start-register-background', 'start-register-background-none')
-}
-
-function loadRegisterTimeoutIndex() {
-    let startregisterbackground = document.getElementById('start_register_background')
-    startregisterbackground.style.zIndex = -1;
-}
 
 function signUpBox() {
     let loginpwimg = document.getElementById('register_pw_login_img')
