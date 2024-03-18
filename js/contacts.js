13d3d12e50082cac95d45b473842886f28d80ee7
Simon Kral <simonkral92@gmail.com>
2024-03-18 17:28:58 +0100

    added email to contacts

--- js/contacts.js
+++ js/contacts.js
@@ -1,15 +1,15 @@
 let allcontacts = [
     { idcounter: 9 },
-    { id: 0, firstname: 'Sofia', lastname: 'Müller (You)', color: '#00BEE8' },
-    { id: 1, firstname: 'Anton', lastname: 'Mayer', color: '#FF7A00' },
-    { id: 2, firstname: 'Anja', lastname: 'Schulz', color: '#9327FF' },
-    { id: 3, firstname: 'Benedikt', lastname: 'Ziegler', color: '#6E52FF' },
-    { id: 4, firstname: 'David', lastname: 'Eisenberg', color: '#FC71FF' },
-    { id: 5, firstname: 'Eva', lastname: 'Fischer', color: '#FFBB2B' },
-    { id: 6, firstname: 'Emmanuel', lastname: 'Mauer', color: '#1FD7C1' },
-    { id: 7, firstname: 'Marcel', lastname: 'Bauer', color: '#462F8A' },
-    { id: 8, firstname: 'Tatjana', lastname: 'Wolf', color: '#FF4646' },
-    { id: 9, firstname: 'Tatjana', lastname: 'Wolf', color: '#FF4646' }
+    { id: 0, firstname: 'Sofia', lastname: 'Müller (You)', email: 'sofiam@gmail.com', color: '#00BEE8' },
+    { id: 1, firstname: 'Anton', lastname: 'Mayer', email: 'antom@gmail.com', color: '#FF7A00' },
+    { id: 2, firstname: 'Anja', lastname: 'Schulz', email: 'schulz@hotmail.com', color: '#9327FF' },
+    { id: 3, firstname: 'Benedikt', lastname: 'Ziegler', email: 'benedikt@gmail.com', color: '#6E52FF' },
+    { id: 4, firstname: 'David', lastname: 'Eisenberg', email: 'davidberg@gmail.com', color: '#FC71FF' },
+    { id: 5, firstname: 'Eva', lastname: 'Fischer', email: 'eva@gmail.com', color: '#FFBB2B' },
+    { id: 6, firstname: 'Emmanuel', lastname: 'Mauer', email: 'emmanuelma@gmail.com', color: '#1FD7C1' },
+    { id: 7, firstname: 'Marcel', lastname: 'Bauer', email: 'bauer@gmail.com', color: '#462F8A' },
+    { id: 8, firstname: 'Tatjana', lastname: 'Wolf', email: 'wolf@gmail.com', color: '#FF4646' },
+    { id: 9, firstname: 'Tatjana', lastname: 'Wolf', email: 'wolf@gmail.com', color: '#FF4646' }
 ];
 
 function initContacts() {
