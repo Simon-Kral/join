4e33292029bcfc7b10b23dcab91b1833579a2fcb
Simon Kral <simonkral92@gmail.com>
2024-04-03 18:54:26 +0200

    bugfix renderCategory-Function

--- js/board.js
+++ js/board.js
@@ -4,7 +4,6 @@ let currentdragged;
 let currentdraggedarray;
 let task;
 
-
 function boardinit() {
     renderTodo();
     renderInProgress();
@@ -17,69 +16,72 @@ function boardinit() {
 }
 
 function showEmptyHtmlTodo() {
-    if (users[loaduser]['todo'] == 0) {
+    if (users[loaduser]["todo"] == 0) {
         const empty = emptyTaskFieldTodo();
-        let getplacetodo = document.getElementById('to_do_place');
+        let getplacetodo = document.getElementById("to_do_place");
         getplacetodo.innerHTML = empty;
     }
 }
 
 function showEmptyHtmlInprogress() {
-    if (users[loaduser]['tasksinprogress'] == 0) {
+    if (users[loaduser]["tasksinprogress"] == 0) {
         const empty = emptyTaskFieldInprogress();
-        let getplacetodo = document.getElementById('in_progress_place');
+        let getplacetodo = document.getElementById("in_progress_place");
         getplacetodo.innerHTML = empty;
     }
 }
 
 function showEmptyHtmlAwaitfeedback() {
-    if (users[loaduser]['awaitingfeedback'] == 0) {
+    if (users[loaduser]["awaitingfeedback"] == 0) {
         const empty = emptyTaskFieldAwaitfeedback();
-        let getplacetodo = document.getElementById('await_feedback_place');
+        let getplacetodo = document.getElementById("await_feedback_place");
         getplacetodo.innerHTML = empty;
     }
 }
 
 function showEmptyHtmlDone() {
-    if (users[loaduser]['done'] == 0) {
+    if (users[loaduser]["done"] == 0) {
         const empty = emptyTaskFieldDone();
-        let getemptyplacedone = document.getElementById('done_place');
+        let getemptyplacedone = document.getElementById("done_place");
         getemptyplacedone.innerHTML = empty;
     }
 }
 
 function closeCard() {
-    document.getElementById('fullscreen_information').classList.add('d-none');
+    document.getElementById("fullscreen_information").classList.add("d-none");
     boardinit();
 }
 
 function bordAddNewTask() {
     const card = bordAddTaskFieldHtml();
-    let getplacecard = document.getElementById('add_bordtask_data');
-    document.getElementById('fullscreen_information').classList.remove('d-none');
+    let getplacecard = document.getElementById("add_bordtask_data");
+    document.getElementById("fullscreen_information").classList.remove("d-none");
     getplacecard.innerHTML = card;
     fillAddTaskSection();
 }
 
 function openBordTask(id, element) {
-    if (element.parentElement && element.parentElement.id === 'to_do_place') {
+    if (element.parentElement && element.parentElement.id === "to_do_place") {
         dragTodo(id);
         openTask(id);
-    } if (element.parentElement && element.parentElement.id === 'in_progress_place') {
+    }
+    if (element.parentElement && element.parentElement.id === "in_progress_place") {
         dragInProgress(id);
         openTask(id);
-    } if (element.parentElement && element.parentElement.id === 'await_feedback_place') {
+    }
+    if (element.parentElement && element.parentElement.id === "await_feedback_place") {
         dragAwaitFeedback(id);
         openTask(id);
-    } if (element.parentElement && element.parentElement.id === 'done_place') {
+    }
+    if (element.parentElement && element.parentElement.id === "done_place") {
         dragDone(id);
         openTask(id);
     }
 }
 
 function openTask(i) {
-    let getplacecard = document.getElementById('add_bordtask_data');
-    document.getElementById('fullscreen_information').classList.remove('d-none');
+    let getplacecard = document.getElementById("add_bordtask_data");
+    document.getElementById("fullscreen_information").classList.remove("d-none");
     const selectarray = currentdraggedarray[i];
     const { categorytodo, titletodo, descriptiontodo, subtaskstodo, contactstodo, priotodo, datetodo } = informationTodo(selectarray);
     const choosencategory = selectCategory(categorytodo);
@@ -91,7 +93,7 @@ function openTask(i) {
 }
 
 function editSingleTask(i) {
-    let getplacetaskvariantbord = document.getElementById('add_bordtask_data');
+    let getplacetaskvariantbord = document.getElementById("add_bordtask_data");
     const selectarray = currentdraggedarray[i];
     const { categorytodo, titletodo, descriptiontodo, subtaskstodo, contactstodo, priotodo, datetodo } = informationTodo(selectarray);
     categorytodo;
@@ -101,13 +103,13 @@ function editSingleTask(i) {
     const selectcontacts = selectContacts(contactstodo);
     getplacetaskvariantbord.innerHTML = ``;
     getplacetaskvariantbord.innerHTML = editTaskHtml(titletodo, descriptiontodo, i, choosenpriority, datetodo, getsubtaskhtml, selectcontacts);
-    initAddTask();
+    initAddTask(i);
 }
 
 function renderTodo() {
-    let getplacetodo = document.getElementById('to_do_place');
+    let getplacetodo = document.getElementById("to_do_place");
     getplacetodo.innerHTML = ``;
-    let todo = users[loaduser]['todo'];
+    let todo = users[loaduser]["todo"];
     for (let i = 0; i < todo.length; i++) {
         todocollect = todo[i];
         showTodoHtml(todocollect, i);
@@ -115,16 +117,16 @@ function renderTodo() {
 }
 
 function showTodoHtml(getinformationtodo, i) {
-    let getplacetodo = document.getElementById('to_do_place');
+    let getplacetodo = document.getElementById("to_do_place");
     const { categorytodo, titletodo, descriptiontodo, subtaskstodo, contactstodo, priotodo } = informationTodo(getinformationtodo);
     const { choosencategory, choosenpriority, selectedCheckboxCount, barupdated, subtasklength, selectcontacts } = informationsFactory(categorytodo, priotodo, subtaskstodo, contactstodo);
     getplacetodo.innerHTML += todoTaskHtml(choosencategory, titletodo, descriptiontodo, i, barupdated, selectcontacts, choosenpriority, subtasklength, selectedCheckboxCount);
 }
 
 function renderInProgress() {
-    let getplacetodo = document.getElementById('in_progress_place');
+    let getplacetodo = document.getElementById("in_progress_place");
     getplacetodo.innerHTML = ``;
-    let inprogress = users[loaduser]['tasksinprogress'];
+    let inprogress = users[loaduser]["tasksinprogress"];
     for (let i = 0; i < inprogress.length; i++) {
         inprogresscollect = inprogress[i];
         showInProgressHtml(inprogresscollect, i);
@@ -139,9 +141,9 @@ function showInProgressHtml(inprogresscollect, i) {
 }
 
 function renderAwaitFeedback() {
-    let getplacetodo = document.getElementById('await_feedback_place');
+    let getplacetodo = document.getElementById("await_feedback_place");
     getplacetodo.innerHTML = ``;
-    let awaitfeedback = users[loaduser]['awaitingfeedback'];
+    let awaitfeedback = users[loaduser]["awaitingfeedback"];
     for (let i = 0; i < awaitfeedback.length; i++) {
         awaitfeedbackcollect = awaitfeedback[i];
         showAwaitFeedbackHtml(awaitfeedbackcollect, i);
@@ -149,16 +151,16 @@ function renderAwaitFeedback() {
 }
 
 function showAwaitFeedbackHtml(awaitfeedbackcollect, i) {
-    let getplaceawaitfeedback = document.getElementById('await_feedback_place');
+    let getplaceawaitfeedback = document.getElementById("await_feedback_place");
     const { categorytodo, titletodo, descriptiontodo, subtaskstodo, contactstodo, priotodo } = informationTodo(awaitfeedbackcollect);
     const { choosencategory, choosenpriority, selectedCheckboxCount, barupdated, subtasklength, selectcontacts } = informationsFactory(categorytodo, priotodo, subtaskstodo, contactstodo);
     getplaceawaitfeedback.innerHTML += todoTaskHtml(choosencategory, titletodo, descriptiontodo, i, barupdated, selectcontacts, choosenpriority, subtasklength, selectedCheckboxCount);
 }
 
 function renderDone() {
-    let getplacetodo = document.getElementById('done_place');
+    let getplacetodo = document.getElementById("done_place");
     getplacetodo.innerHTML = ``;
-    let done = users[loaduser]['done'];
+    let done = users[loaduser]["done"];
     for (let i = 0; i < done.length; i++) {
         donecollect = done[i];
         showDoneHtml(donecollect, i);
@@ -166,20 +168,20 @@ function renderDone() {
 }
 
 function showDoneHtml(donecollect, i) {
-    let getplacedone = document.getElementById('done_place');
+    let getplacedone = document.getElementById("done_place");
     const { categorytodo, titletodo, descriptiontodo, subtaskstodo, contactstodo, priotodo } = informationTodo(donecollect);
     const { choosencategory, choosenpriority, selectedCheckboxCount, barupdated, subtasklength, selectcontacts } = informationsFactory(categorytodo, priotodo, subtaskstodo, contactstodo);
     getplacedone.innerHTML += todoTaskHtml(choosencategory, titletodo, descriptiontodo, i, barupdated, selectcontacts, choosenpriority, subtasklength, selectedCheckboxCount);
 }
 
 function informationTodo(getinformationtodo) {
-    let categorytodo = getinformationtodo['category'];
-    let titletodo = getinformationtodo['title'];
-    let descriptiontodo = getinformationtodo['description'];
-    let subtaskstodo = getinformationtodo['subtasks'];
-    let contactstodo = getinformationtodo['contacts'];
-    let priotodo = getinformationtodo['prio'];
-    let datetodo = getinformationtodo['date'];
+    let categorytodo = getinformationtodo["category"];
+    let titletodo = getinformationtodo["title"];
+    let descriptiontodo = getinformationtodo["description"];
+    let subtaskstodo = getinformationtodo["subtasks"];
+    let contactstodo = getinformationtodo["contacts"];
+    let priotodo = getinformationtodo["prio"];
+    let datetodo = getinformationtodo["date"];
     return { categorytodo, titletodo, descriptiontodo, subtaskstodo, contactstodo, priotodo, datetodo };
 }
 
@@ -211,10 +213,12 @@ function selectPriority(priority) {
     if (priority == "urgent") {
         let urugentprio = urugentPrioHtml();
         return urugentprio;
-    } if (priority = "low") {
+    }
+    if ((priority = "low")) {
         let lowprio = lowPrioHtml();
         return lowprio;
-    } if (priority = "medium") {
+    }
+    if ((priority = "medium")) {
         let mediumprio = mediumPrioHtml();
         return mediumprio;
     }
@@ -222,7 +226,7 @@ function selectPriority(priority) {
 
 function showTaskCategoryBig(category) {
     const html = category === "Technical Task" ? technicalTaskHtml() : userTaskHtml();
-    document.getElementById('task_variant_bord').innerHTML = html;
+    document.getElementById("task_variant_bord").innerHTML = html;
 }
 
 function allowDrop(ev) {
@@ -230,19 +234,22 @@ function allowDrop(ev) {
 }
 
 function drag(id, element) {
-    if (element.parentElement && element.parentElement.id === 'to_do_place') {
+    if (element.parentElement && element.parentElement.id === "to_do_place") {
         dragTodo(id);
-    } if (element.parentElement && element.parentElement.id === 'in_progress_place') {
+    }
+    if (element.parentElement && element.parentElement.id === "in_progress_place") {
         dragInProgress(id);
-    } if (element.parentElement && element.parentElement.id === 'await_feedback_place') {
+    }
+    if (element.parentElement && element.parentElement.id === "await_feedback_place") {
         dragAwaitFeedback(id);
-    } if (element.parentElement && element.parentElement.id === 'done_place') {
+    }
+    if (element.parentElement && element.parentElement.id === "done_place") {
         dragDone(id);
     }
 }
 
 function updateProgressBar(subtasks, selectedCheckboxCount) {
-    let percent = selectedCheckboxCount === 1 ? 0 : Math.round(selectedCheckboxCount / subtasks.length * 100);
+    let percent = selectedCheckboxCount === 1 ? 0 : Math.round((selectedCheckboxCount / subtasks.length) * 100);
     return subtasks <= 0 ? emptyPlaceHtml() : createProgressBar(percent);
 }
 
@@ -250,7 +257,7 @@ function selectSubtasks(subtaskstodo) {
     let iteratedList = [];
     for (let st = 1; st < subtaskstodo.length; st++) {
         let sublistplace = subtaskstodo[st];
-        let list = sublistplace['name'];
+        let list = sublistplace["name"];
         iteratedList.push(list);
     }
     return iteratedList;
@@ -305,30 +312,30 @@ function dragDone(id) {
 }
 
 function setTodoArray() {
-    let selecttodo = users[loaduser]['todo'];
+    let selecttodo = users[loaduser]["todo"];
     checkboxtodo = selecttodo;
     return selecttodo;
 }
 
 function setInProgressArray() {
-    let selectinprogress = users[loaduser]['tasksinprogress'];
+    let selectinprogress = users[loaduser]["tasksinprogress"];
     return selectinprogress;
 }
 
 function setAwaitFeedbackArray() {
-    let selectafeedback = users[loaduser]['awaitingfeedback'];
+    let selectafeedback = users[loaduser]["awaitingfeedback"];
     return selectafeedback;
 }
 
 function setDoneForm() {
-    let selectdone = users[loaduser]['done'];
+    let selectdone = users[loaduser]["done"];
     return selectdone;
 }
 
 async function dropTodo(ev) {
     ev.preventDefault();
     let select = users[loaduser];
-    select['todo'].push(currentdraggedarray[currentdragged]);
+    select["todo"].push(currentdraggedarray[currentdragged]);
     currentdraggedarray.splice(currentdragged, 1);
     boardinit();
     await saveToServer();
@@ -337,7 +344,7 @@ async function dropTodo(ev) {
 async function dropInProgress(ev) {
     ev.preventDefault();
     let select = users[loaduser];
-    select['tasksinprogress'].push(currentdraggedarray[currentdragged]);
+    select["tasksinprogress"].push(currentdraggedarray[currentdragged]);
     currentdraggedarray.splice(currentdragged, 1);
     boardinit();
     await saveToServer();
@@ -346,7 +353,7 @@ async function dropInProgress(ev) {
 async function dropAwaitFeedBack(ev) {
     ev.preventDefault();
     let select = users[loaduser];
-    select['awaitingfeedback'].push(currentdraggedarray[currentdragged]);
+    select["awaitingfeedback"].push(currentdraggedarray[currentdragged]);
     currentdraggedarray.splice(currentdragged, 1);
     boardinit();
     await saveToServer();
@@ -355,7 +362,7 @@ async function dropAwaitFeedBack(ev) {
 async function dropDone(ev) {
     ev.preventDefault();
     let select = users[loaduser];
-    select['done'].push(currentdraggedarray[currentdragged]);
+    select["done"].push(currentdraggedarray[currentdragged]);
     currentdraggedarray.splice(currentdragged, 1);
     boardinit();
     await saveToServer();
@@ -370,12 +377,12 @@ function searchTaskPlace() {
         todoplace: document.getElementById(`to_do_place`),
         inprogressplace: document.getElementById(`in_progress_place`),
         awaitfeedbackplace: document.getElementById(`await_feedback_place`),
-        doneplace: document.getElementById(`done_place`)
+        doneplace: document.getElementById(`done_place`),
     };
 }
 
 function getSearchInput() {
-    const search = document.getElementById('search_input').value.trim().toLowerCase();
+    const search = document.getElementById("search_input").value.trim().toLowerCase();
     return { search, searchlength: search.length };
 }
 
@@ -395,12 +402,12 @@ function filterResults(search) {
         resultstodo: filterArray(setTodoArray(), search),
         resultsinprogress: filterArray(setInProgressArray(), search),
         resultsawaitfeedback: filterArray(setAwaitFeedbackArray(), search),
-        resultsdone: filterArray(setDoneForm(), search)
+        resultsdone: filterArray(setDoneForm(), search),
     };
 }
 
 function filterArray(array, search) {
-    return array.filter(item => item.title.toLowerCase().includes(search));
+    return array.filter((item) => item.title.toLowerCase().includes(search));
 }
 
 function renderResults(filteredResults, todoplace, inprogressplace, awaitfeedbackplace, doneplace) {
@@ -411,7 +418,7 @@ function renderResults(filteredResults, todoplace, inprogressplace, awaitfeedbac
 }
 
 function renderResultsCategory(results, place) {
-    place.innerHTML = '';
+    place.innerHTML = "";
     if (results.length <= 0) {
         place.innerHTML = nothingFound();
     } else {
@@ -421,16 +428,16 @@ function renderResultsCategory(results, place) {
 
 function showHtml(item, place, index) {
     switch (place.id) {
-        case 'to_do_place':
+        case "to_do_place":
             showTodoHtml(item, index);
             break;
-        case 'in_progress_place':
+        case "in_progress_place":
             showInProgressHtml(item, index);
             break;
-        case 'await_feedback_place':
+        case "await_feedback_place":
             showAwaitFeedbackHtml(item, index);
             break;
-        case 'done_place':
+        case "done_place":
             showDoneHtml(item, index);
             break;
     }
